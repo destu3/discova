ec78f67e134c3a82ea5a537b573b5e7f1c734d01
destu3 <obose03@gmail.com>
2022-11-14 13:49:51 +0000

    init commit
    project set up

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+console.log('Web pack set up');
+
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
